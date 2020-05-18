a703071fdc7b2189bf47a6542543569b01361f7f
fazrisuhada007 <fazrisuhada3@gmail.com>
2020-05-18 16:08:58 +0700

    Menu login dan dashboard admin

---
+++ application/views/dashboard.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+
+<!-- 
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
+
+<!-- begin::Head -->
+
+<head>
+	<meta charset="utf-8" />
+	<title>Metronic | Dashboard</title>
+	<meta name="description" content="Latest updates and statistic charts">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+
+	<!--begin::Web font -->
+	<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+	<script>
+		WebFont.load({
+			google: {
+				"families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]
+			},
+			active: function() {
+				sessionStorage.fonts = true;
+			}
+		});
+	</script>
+
+	<!--end::Web font -->
+
+	<!--begin::Global Theme Styles -->
+	<link href="<?php base_url() ?>assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
+
+	<!--RTL version:<link href="<?php base_url() ?>assets/vendors/base/vendors.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+	<link href="<?php base_url() ?>assets/demo/demo11/base/style.bundle.css" rel="stylesheet" type="text/css" />
+
+	<!--RTL version:<link href="<?php base_url() ?>assets/demo/demo11/base/style.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+
+	<!--end::Global Theme Styles -->
+
+	<!--begin::Page Vendors Styles -->
+	<link href="<?php base_url() ?>assets/vendors/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
+
+	<!--RTL version:<link href="<?php base_url() ?>assets/vendors/custom/fullcalendar/fullcalendar.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+
+	<!--end::Page Vendors Styles -->
+	<link rel="shortcut icon" href="<?php base_url() ?>assets/demo/demo11/media/img/logo/favicon.ico" />
+</head>
+<!-- begin::Body -->
+<body class="m-content--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-light m-aside--offcanvas-default">
+<!-- begin:: Page -->
+<div class="m-grid m-grid--hor m-grid--root m-page">
+<!-- BEGIN: Header -->
+<header id="m_header" class="m-grid__item    m-header " m-minimize-offset="200" m-minimize-mobile-offset="200">
+<div class="m-container m-container--fluid m-container--full-height">
+<div class="m-stack m-stack--ver m-stack--desktop">
+	<!-- BEGIN: Brand -->
+	<div class="m-stack__item m-brand  m-brand--skin-light ">
+		<div class="m-stack m-stack--ver m-stack--general m-stack--fluid">
+			<div class="m-stack__item m-stack__item--middle m-brand__logo">
+				<a href="index.html" class="m-brand__logo-wrapper">
+					<img alt="" src="<?php base_url() ?>assets/demo/demo11/media/img/logo/logo.png" />
+				</a>
+			</div>
+			<div class="m-stack__item m-stack__item--middle m-brand__tools">
+				<div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-left m-dropdown--align-push" m-dropdown-toggle="click" aria-expanded="true">
+				</div>
+				<!-- BEGIN: Responsive Aside Left Menu Toggler -->
+				<a href="javascript:;" id="m_aside_left_offcanvas_toggle" class="m-brand__icon m-brand__toggler m-brand__toggler--left m--visible-tablet-and-mobile-inline-block">
+					<span></span>
+				</a>
+				<!-- END -->
+				<!-- BEGIN: Topbar Toggler -->
+				<a id="m_aside_header_topbar_mobile_toggle" href="javascript:;" class="m-brand__icon m--visible-tablet-and-mobile-inline-block">
+					<i class="flaticon-more"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<!-- END: Brand -->
+	<div class="m-stack__item m-stack__item--fluid m-header-head" id="m_header_nav">
+
+		<!-- BEGIN: Topbar -->
+		<div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
+			<div class="m-stack__item m-topbar__nav-wrapper">
+				<ul class="m-topbar__nav m-nav m-nav--inline">
+					<li class="m-nav__item" style=" display: block; padding-top: 21px; ">
+						<a href="<?php echo base_url('login/logout') ?>" class="btn btn-outline-brand m-btn btn-sm">
+							<B>Logout</B>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+</div>
+</div>
+</header>
+
+	<!-- begin::Body -->
+	<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+		<!-- BEGIN: Left Aside -->
+		<button class="m-aside-left-close  m-aside-left-close--skin-light " id="m_aside_left_close_btn"><i class="la la-close"></i></button>
+		<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-light ">
+
+			<!-- BEGIN: Aside Menu -->
+			<div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-light m-aside-menu--submenu-skin-light " m-menu-vertical="1" m-menu-scrollable="0" m-menu-dropdown-timeout="500">
+				<ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
+					<li class="m-menu__section m-menu__section--first">
+						<h4 class="m-menu__section-text">Main Navigation</h4>
+						<i class="m-menu__section-icon flaticon-more-v2"></i>
+					</li>
+					<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><i class="m-menu__link-icon flaticon-layers"></i><span class="m-menu__link-text">Dashboard</span></a></li>
+					<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><i class="m-menu__link-icon flaticon-truck"></i><span class="m-menu__link-text">Suppliers</span></a></li>
+					<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><i class="m-menu__link-icon flaticon-customer"></i><span class="m-menu__link-text">Customers</span></a></li>
+					<li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover" m-menu-link-redirect="1"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-open-box"></i><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Products</span> <span class="m-menu__link-badge"></span> </span></span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
+						<div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+							<ul class="m-menu__subnav">
+								<li class="m-menu__item  m-menu__item--parent" aria-haspopup="true" m-menu-link-redirect="1"><span class="m-menu__link"><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Products</span> <span class="m-menu__link-badge"></span> </span></span></span></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Categories</span></a></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Units</span></a></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Items</span></a></li>
+							</ul>
+						</div>
+					</li>
+					<li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover" m-menu-link-redirect="1"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-cart"></i><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Transaction</span> <span class="m-menu__link-badge"></span> </span></span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
+						<div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+							<ul class="m-menu__subnav">
+								<li class="m-menu__item  m-menu__item--parent" aria-haspopup="true" m-menu-link-redirect="1"><span class="m-menu__link"><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Transaction</span> <span class="m-menu__link-badge"></span> </span></span></span></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Sales</span></a></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Stock In</span></a></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Stock Out</span></a></li>
+							</ul>
+						</div>
+					</li>
+					<li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover" m-menu-link-redirect="1"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon flaticon-graphic-1"></i><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Reports</span> <span class="m-menu__link-badge"></span> </span></span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
+						<div class="m-menu__submenu "><span class="m-menu__arrow"></span>
+							<ul class="m-menu__subnav">
+								<li class="m-menu__item  m-menu__item--parent" aria-haspopup="true" m-menu-link-redirect="1"><span class="m-menu__link"><span class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span class="m-menu__link-text">Reports</span> <span class="m-menu__link-badge"><span class="m-badge m-badge--accent">3</span></span> </span></span></span></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Sales</span></a></li>
+								<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><span class="m-menu__link-text">Stock</span></a></li>
+							</ul>
+						</div>
+					</li>
+					<li class="m-menu__section ">
+						<h4 class="m-menu__section-text">System</h4>
+						<i class="m-menu__section-icon flaticon-more-v2"></i>
+					</li>
+					<li class="m-menu__item " aria-haspopup="true" m-menu-link-redirect="1"><a href="inner.html" class="m-menu__link "><i class="m-menu__link-icon flaticon-users"></i><span class="m-menu__link-text">Users</span></a></li>
+				</ul>
+			</div>
+
+			<!-- END: Aside Menu -->
+		</div>
+
+		<!-- END: Left Aside -->
+		<div class="m-grid__item m-grid__item--fluid m-wrapper">
+
+			<!-- BEGIN: Subheader -->
+			<div class="m-subheader ">
+				<div class="d-flex align-items-center">
+					<div class="mr-auto">
+						<h3 class="m-subheader__title ">Dashboard</h3>
+					</div>
+				</div>
+			</div>
+
+			<!-- END: Subheader -->
+			<div class="m-content">
+
+				<!--begin:: Widgets/Stats-->
+				<div class="m-portlet ">
+					<div class="m-portlet__body  m-portlet__body--no-padding">
+						<div class="row m-row--no-padding m-row--col-separator-xl">
+							<div class="col-md-12 col-lg-6 col-xl-3">
+
+								<!--begin::Total Profit-->
+								<div class="m-widget24">
+									<div class="m-widget24__item">
+										<h4 class="m-widget24__title">
+											Total Frofit
+										</h4><br>
+										<span class="m-widget24__desc">
+											All Customs Value
+										</span>
+										<span class="m-widget24__stats m--font-brand">
+											$18M
+										</span>
+										<div class="m--space-10"></div>
+										<div class="progress m-progress--sm">
+											<div class="progress-bar m--bg-brand" role="progressbar" style="width: 78%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="m-widget24__change">
+											Change
+										</span>
+										<span class="m-widget24__number">
+											78%
+										</span>
+									</div>
+								</div>
+
+								<!--end::Total Profit-->
+							</div>
+							<div class="col-md-12 col-lg-6 col-xl-3">
+
+								<!--begin::New Feedbacks-->
+								<div class="m-widget24">
+									<div class="m-widget24__item">
+										<h4 class="m-widget24__title">
+											New Feedbacks
+										</h4><br>
+										<span class="m-widget24__desc">
+											Customer Review
+										</span>
+										<span class="m-widget24__stats m--font-info">
+											1349
+										</span>
+										<div class="m--space-10"></div>
+										<div class="progress m-progress--sm">
+											<div class="progress-bar m--bg-info" role="progressbar" style="width: 84%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="m-widget24__change">
+											Change
+										</span>
+										<span class="m-widget24__number">
+											84%
+										</span>
+									</div>
+								</div>
+
+								<!--end::New Feedbacks-->
+							</div>
+							<div class="col-md-12 col-lg-6 col-xl-3">
+
+								<!--begin::New Orders-->
+								<div class="m-widget24">
+									<div class="m-widget24__item">
+										<h4 class="m-widget24__title">
+											New Orders
+										</h4><br>
+										<span class="m-widget24__desc">
+											Fresh Order Amount
+										</span>
+										<span class="m-widget24__stats m--font-danger">
+											567
+										</span>
+										<div class="m--space-10"></div>
+										<div class="progress m-progress--sm">
+											<div class="progress-bar m--bg-danger" role="progressbar" style="width: 69%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="m-widget24__change">
+											Change
+										</span>
+										<span class="m-widget24__number">
+											69%
+										</span>
+									</div>
+								</div>
+
+								<!--end::New Orders-->
+							</div>
+							<div class="col-md-12 col-lg-6 col-xl-3">
+
+								<!--begin::New Users-->
+								<div class="m-widget24">
+									<div class="m-widget24__item">
+										<h4 class="m-widget24__title">
+											New Users
+										</h4><br>
+										<span class="m-widget24__desc">
+											Joined New User
+										</span>
+										<span class="m-widget24__stats m--font-success">
+											276
+										</span>
+										<div class="m--space-10"></div>
+										<div class="progress m-progress--sm">
+											<div class="progress-bar m--bg-success" role="progressbar" style="width: 90%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+										</div>
+										<span class="m-widget24__change">
+											Change
+										</span>
+										<span class="m-widget24__number">
+											90%
+										</span>
+									</div>
+								</div>
+								<!--end::New Users-->
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- begin::Footer -->
+				<footer class="m-grid__item		m-footer ">
+					<div class="m-container m-container--fluid m-container--full-height m-page__container">
+						<div class="m-stack m-stack--flex-tablet-and-mobile m-stack--ver m-stack--desktop">
+							<div class="m-stack__item m-stack__item--left m-stack__item--middle m-stack__item--last">
+								<span class="m-footer__copyright">
+									2017 &copy; Metronic theme by <a href="https://keenthemes.com" class="m-link">Keenthemes</a>
+								</span>
+							</div>
+						</div>
+					</div>
+				</footer>
+				<!-- end::Footer -->
+			</div>
+
+			<!-- begin::Scroll Top -->
+			<div id="m_scroll_top" class="m-scroll-top">
+				<i class="la la-arrow-up"></i>
+			</div>
+			<!--begin::Global Theme Bundle -->
+			<script src="<?php base_url() ?>assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+			<script src="<?php base_url() ?>assets/demo/demo11/base/scripts.bundle.js" type="text/javascript"></script>
+
+			<!--end::Global Theme Bundle -->
+
+			<!--begin::Page Vendors -->
+			<script src="<?php base_url() ?>assets/vendors/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+
+			<!--end::Page Vendors -->
+
+			<!--begin::Page Scripts -->
+			<script src="<?php base_url() ?>assets/app/js/dashboard.js" type="text/javascript"></script>
+
+			<!--end::Page Scripts -->
+</body>
+
+<!-- end::Body -->
+
+</html>
---
+++ application/views/login.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+
+<!-- 
+Template Name: Metronic - Responsive Admin Dashboard Template build with Twitter Bootstrap 4
+Author: KeenThemes
+Website: http://www.keenthemes.com/
+Contact: support@keenthemes.com
+Follow: www.twitter.com/keenthemes
+Dribbble: www.dribbble.com/keenthemes
+Like: www.facebook.com/keenthemes
+Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
+License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
+-->
+<html lang="en">
+
+    <!-- begin::Head -->
+    <head>
+        <meta charset="utf-8" />
+        <title>Metronic | Login Page - 5</title>
+        <meta name="description" content="Latest updates and statistic charts">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+
+        <!--begin::Web font -->
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+        <script>
+            WebFont.load({
+            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+          });
+        </script>
+
+        <!--end::Web font -->
+
+        <!--begin::Global Theme Styles -->
+        <link href="<?php echo base_url() ?>assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
+
+        <!--RTL version:<link href="<?php echo base_url() ?>assets/vendors/base/vendors.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+        <link href="<?php echo base_url() ?>assets/demo/default/base/style.bundle.css" rel="stylesheet" type="text/css" />
+
+        <!--RTL version:<link href="<?php echo base_url() ?>assets/demo/default/base/style.bundle.rtl.css" rel="stylesheet" type="text/css" />-->
+
+        <!--end::Global Theme Styles -->
+        <link rel="shortcut icon" href="<?php echo base_url() ?>assets/demo/default/media/img/logo/favicon.ico" />
+    </head>
+
+    <!-- end::Head -->
+
+    <!-- begin::Body -->
+    <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
+
+        <!-- begin:: Page -->
+        <div class="m-grid m-grid--hor m-grid--root m-page">
+            <div class="m-login m-login--signin  m-login--5" id="m_login" style="background-image: url(<?php echo base_url() ?>assets/app/media/img/bg/bg-3.jpg);">
+                <div class="m-login__wrapper-1 m-portlet-full-height">
+                    <div class="m-login__wrapper-1-1">
+                        <div class="m-login__contanier">
+                            <div class="m-login__content">
+                                <div class="m-login__logo">
+                                    <a href="#">
+                                        <img src="<?php echo base_url() ?>assets/app/media/img/logos/alfamart.png">
+                                    </a>
+                                </div>
+                                <div class="m-login__title">
+                                    <h3> “Belanja Puas, Harga Pas Ya di Alfamart”</h3>
+                                </div>
+                                <div class="m-login__desc">
+                                    Point Of Sale System
+                                </div>
+                                <div class="m-login__form-action">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div class="m-login__border">
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="m-login__wrapper-2 m-portlet-full-height">
+                    <div class="m-login__contanier">
+                        <div class="m-login__signin">
+                            <div class="m-login__head">
+                                <h3 class="m-login__title">Masuk ke akun Anda</h3>
+                            </div>
+                            <form class="m-login__form m-form" action="">
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input" type="text" placeholder="Username" name="username" autocomplete="off">
+                                </div>
+                                <div class="form-group m-form__group">
+                                    <input class="form-control m-input m-login__form-input--last" type="Password" placeholder="Password" name="password">
+                                </div>
+                                <div class="row m-login__form-sub">
+                                    <!-- <div class="col m--align-left">
+                                        <label class="m-checkbox m-checkbox--focus">
+                                            <input type="checkbox" name="remember"> Remember me
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                    <div class="col m--align-right">
+                                        <a href="javascript:;" id="m_login_forget_password" class="m-link">Forget Password ?</a>
+                                    </div> -->
+                                </div>
+                                <div class="m-login__form-action" style="margin-top: 2px;">
+                                    <button id="m_login_signin_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- end:: Page -->
+
+        <!--begin::Global Theme Bundle -->
+        <script src="<?php echo base_url() ?>assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+        <script src="<?php echo base_url() ?>assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
+
+        <!--end::Global Theme Bundle -->
+
+        <!--begin::Page Scripts -->
+        <script src="<?php echo base_url() ?>assets/snippets/custom/pages/user/login.js" type="text/javascript"></script>
+
+        <!--end::Page Scripts -->
+    </body>
+
+    <!-- end::Body -->
+</html>
